07eac49c53f6413c04f844e3db9a76deb1570915
SmithEcophysLab <nick.gregory.smith@gmail.com>
2022-10-14 12:51:09 -0500

    i think this is it, but need to check the j stuff as still seems a bit off

--- model_code/photosynthesis_model.R
+++ model_code/photosynthesis_model.R
@@ -13,22 +13,68 @@ library(R.utils)
 # load necessary functions
 sourceDirectory('functions', modifiedOnly = FALSE)
 
-photosynthesis_model <- function(){
+photosynthesis_model <- function(elevation_m = 0, ca_ppm = 420, temperature_c = 25, par = 400,
+                                 vcmax25 = 100, jmax25 = 100,
+                                 phi_psii = 0.6895, # rate at 25C from Bernacchi
+                                 e_partitioning_coef = 4, 
+                                 absorbance = 0.85, 
+                                 photosystem_partitioning_coef = 0.5,
+                                 theta = 0.85,
+                                 phi_psii_tresp = 'no'){
   
-  patm <- calc_patm(z)
-  par <- calc_par(paro, z)
-  vpd <- calc_vpd(tg_c, z, vpdo)
-  ca <- cao * 1e-6 * patm
+  patm_pa <- calc_patm(elevation_m) # atmospheric pressure (Pa)
+  ca_pa <- ca_ppm * 1e-6 * patm_pa # atmospheric co2 (Pa)
   
-  ci <- 0.7 * ca
-  gammastar <- calc_gammastar_pa(temperature_c, elevation)
-  km <- calc_km_pa(temperature_c, elevation)
-  mc = (ci - gammastar) / (ci + km)
+  vcmax <- vcmax25 * calc_vcmax_tresp_mult(tleaf = temperature_c, tmean = temperature_c, tref = 25)
+  ci_pa <- 0.7 * ca_pa # intercellular co2 (Pa)
+  gammastar_pa <- calc_gammastar_pa(temperature_c, elevation_m) # co2 compensation point (Pa)
+  km_pa <- calc_km_pa(temperature_c, elevation_m) # michaelis-menten coefficient for rubisco (Pa)
+  mc = (ci_pa - gammastar_pa) / (ci_pa + km_pa) # 
   ac = vcmax * mc # rubisco-limited photosynthesis
   
+  if(phi_psii_tresp == "yes"){
+    # Bernacchi et al. (2003) temperature response (set to 0.257 at 25C)
+    phi_psii = 0.352 + (0.022 * temperature_c) - (0.00034 * temperature_c * temperature_c)
+  }else{
+    phi_psii
+  }
   
-  aj = (j/4) * m
+  jmax <- jmax25 * calc_jmax_tresp_mult(tleaf = temperature_c, tmean = temperature_c, tref = 25)
+  m <- (ci_pa - gammastar_pa) / (ci_pa + 2 * gammastar_pa)
+  j_a <- theta
+  j_b <- -(phi_psii * absorbance * photosystem_partitioning_coef * par + jmax) 
+  j_c <- phi_psii * absorbance * photosystem_partitioning_coef * par * jmax
+  j <- (-j_b + sqrt(j_b^2 - 4 * j_a * j_c)) / (2 * j_a)
+  aj <- (j/e_partitioning_coef) * m # rubp regeneration-limited photosyntehsis
   
-  a = pmin(ac, aj)
+  a <- pmin(ac, aj)
+  
+  results <- data.frame("elevation_m" = elevation_m,
+                        "ca_ppm" = ca_ppm,
+                        "temperature_c" = temperature_c,
+                        "par" = par,
+                        "vcmax" = vcmax,
+                        "jmax" = jmax,
+                        "phi_psii" = phi_psii,
+                        "e_partitioning_coef" = e_partitioning_coef, 
+                        "absorbance" = e_partitioning_coef, 
+                        "photosystem_partitioning_coef" = photosystem_partitioning_coef,
+                        "theta" = theta,
+                        "patm_pa" = patm_pa,
+                        "ca_pa" = ca_pa,
+                        "ci_pa" = ci_pa,
+                        "gammastar_pa" = gammastar_pa,
+                        "km_pa" = km_pa,
+                        "mc" = mc,
+                        "ac" = ac,
+                        "m" = m,
+                        "j_a" = j_a,
+                        "j_b" = j_b,
+                        "j_c" = j_c,
+                        "j" = j,
+                        "aj" = aj,
+                        "a" = a)
+  
+  results
   
 }
---
+++ model_code/test_photosynthesis_model.R
@@ -0,0 +1,41 @@
+# test_photosynthesis_model.R
+## code to test the photosynthesis model
+
+## libraries
+# install.packages('R.utils')
+library(R.utils)
+library(ggplot2)
+
+## load up model and functions
+source('photosynthesis_model.R')
+
+## run model
+
+co2_response <- photosynthesis_model(ca_ppm = seq(200, 2000, 50))
+
+ggplot(data = co2_response, aes(x = ca_ppm, y = a)) +
+  geom_line(col = 'black', size = 2) +
+  geom_line(col = 'red', size = 2, lty = 2, aes(y = aj)) +
+  geom_line(col = 'blue', size = 2, lty = 2, aes(y = ac))
+
+par_response <- photosynthesis_model(par = seq(0, 500, 50))
+
+ggplot(data = par_response, aes(x = par, y = a)) +
+  geom_line(col = 'black', size = 2) +
+  geom_line(col = 'red', size = 2, lty = 2, aes(y = aj)) +
+  geom_line(col = 'blue', size = 2, lty = 2, aes(y = ac))
+
+temperature_response <- photosynthesis_model(temperature_c = seq(0, 50, 5), phi_psii_tresp = "yes")
+
+ggplot(data = temperature_response, aes(x = temperature_c, y = a)) +
+  geom_line(col = 'black', size = 2) +
+  geom_line(col = 'red', size = 2, lty = 2, aes(y = aj)) +
+  geom_line(col = 'blue', size = 2, lty = 2, aes(y = ac))
+
+temperature_response2 <- photosynthesis_model(temperature_c = seq(0, 50, 5), phi_psii_tresp = "no")
+
+ggplot(data = temperature_response2, aes(x = temperature_c, y = a)) +
+  geom_line(col = 'black', size = 2) +
+  geom_line(col = 'red', size = 2, lty = 2, aes(y = aj)) +
+  geom_line(col = 'blue', size = 2, lty = 2, aes(y = ac))
+  
